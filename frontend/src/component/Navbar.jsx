cd8ff7989266484f184ffa3b4c50dac6f70f9683
harshitxarc <harshitverma0010@gmail.com>
2025-05-31 23:16:29 +0530

    Minor commit

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-import './NavBar.css';
+import './Navbar.css';
 
 function NavBar() {
   const [status, setStatus] = useState(false);
